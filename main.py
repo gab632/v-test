54ac8ea0261e7869e4cdd3fb438eb98859777f55
Gabriel <157829529+gab632@users.noreply.github.com>
2025-02-26 07:29:14 +0100

    sync interval variable added

--- main.py
+++ main.py
@@ -8,7 +8,10 @@ from datetime import datetime
 
 def main():
 
+    interval = int(input("Enter synchronization interval in seconds: "))
+
     def replica_func(src, dest):
+        
 
         logger = logging.getLogger(__name__)
         logging.basicConfig(filename='replica_logger.log', encoding='utf-8', level=logging.DEBUG)
@@ -31,7 +34,7 @@ def main():
     times = 5
     while times > 0:
         replica_func(src_folder, r_folder)
-        time.sleep(1)
+        time.sleep(interval)
         if times == 1:
             print()
             answer = input("Continue with the script? (Y/N)")
