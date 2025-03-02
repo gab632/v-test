d02f85d44f56a6e1f7e40907a26ec4bda84aeb8b
Gabriel <157829529+gab632@users.noreply.github.com>
2025-03-02 21:34:01 +0100

    moved the logger code outside the replica function
    Moved the logger code outside of the replica function. 
    Solved the proliferation issue with console logs.

--- main.py
+++ main.py
@@ -10,50 +10,35 @@ import traceback
 
 def main():
 
-    #interval = int(input("Enter synchronization interval in seconds: "))
-    #src_folder = "C:\\Users\\gabri\\learn to code\\python\\replica_project\\demo"
-    #r_folder = "C:\\Users\\gabri\\learn to code\\python\\replica_project\\replica"
-
     try:
-        interval = int(sys.argv[1])
-        src_folder = sys.argv[2]
-        r_folder = sys.argv[3]
-        log_file_path = sys.argv[4]
+        src_folder = sys.argv[1]
+        r_folder = sys.argv[2]
+        log_file_path = sys.argv[3]
+        interval = int(sys.argv[4])
 
     except Exception:
         print("Error while starting the script.")
         sys.exit()
-        
-#    src_folder = "C:\\Users\\local_user\\Downloads\\Source"
-#    r_folder = "C:\\Users\\local_user\\Downloads\\Replica"
 
     create_folders_and_log_file(src_folder, r_folder, log_file_path)
- 
-#    print(hashed_replica_fodler)
-#    print(hashed_src_folder)
-
-    while True:
-        
-        replica_func(src_folder, r_folder, log_file_path)
-        time.sleep(interval)
-
-
-# copy the content of src into dest.
-def replica_func(src, dest, log_path):
 
     logger = logging.getLogger(__name__)
-    logging.basicConfig(filename=log_path, encoding='utf-8', level=logging.DEBUG)
-
+    logging.basicConfig(filename=log_file_path, encoding='utf-8', level=logging.DEBUG)
 
     console = logging.StreamHandler()
     console.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+    formatter = logging.Formatter('%(levelname)-8s %(message)s')
     # tell the handler to use this format
     console.setFormatter(formatter)
     # add the handler to the root logger
-    logging.getLogger('').addHandler(console)
+    logger.addHandler(console)
 
+    while True:     
+        replica_func(src_folder, r_folder, logger)
+        time.sleep(interval)
 
+# copy the content of src folder into dest folder.
+def replica_func(src, dest, logger):
 
     try:
         # copy the file if it is not in the replica folder
@@ -64,7 +49,7 @@ def replica_func(src, dest, log_path):
 
                 shutil.copy2(file_path, dest)
                 # log the operation
-                message = str(datetime.now()), file, ' File copied.'
+                message = str(datetime.now())  + ' File \"' + file + '\" copied.'
                 logger.debug(message)
             else: 
                 src_file_last_modified = os.path.getmtime(file_path)
@@ -73,14 +58,15 @@ def replica_func(src, dest, log_path):
                 dest_file_last_modified = os.path.getmtime(dest_file_path)
                 if not src_file_last_modified == dest_file_last_modified:
                     shutil.copy2(file_path, dest)
-                    message = str(datetime.now()), file, ' File modified.'
+                    message = str(datetime.now()) + ' File \"' + file + '\" modified.'
                     logger.debug(message)
+        # remove the file if it is no longer in the source folder.
         for file in os.listdir(dest):
             if not file in os.listdir(src):
                 os.chdir(dest)
                 file_path = os.getcwd() + '\\' + file
                 os.remove(file_path)  
-                message = str(datetime.now()), file, ' File removed.'
+                message = str(datetime.now()) + ' File \"' + file + '\" removed.'
                 logger.debug(message)
 
 #        shutil.copytree(src, dest, dirs_exist_ok=True)
@@ -91,20 +77,24 @@ def replica_func(src, dest, log_path):
             shutil.copy(src, dest)
         else: 
             traceback.print_exc()
-
+    #console.close()
+    
+    
 # create two folders if they do not exist.
 # create a file given the file path.
 def create_folders_and_log_file(folder1, folder2, file_path):
     try:
         if not os.path.exists(folder1):
             os.makedirs(folder1)
-        
+
         if not os.path.exists(folder2):
             os.makedirs(folder2)
 
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
-    except OSError:
+    
+    except OSError as exc:
         print("Error while creating folders and log file.")
+        print(exc)
         sys.exit()
 
 # hashing function for both files and folders
@@ -135,12 +125,6 @@ def calculate_md5(source):
                     hash_func.update(chunk)
 
     return hash_func.hexdigest()  
-
-
-
-
-
-
 
 main()
     
